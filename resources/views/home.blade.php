9fafb7045d2cc4515b292cb9508cc4504f22d3d6
LukmanFreedom <lukmanfreedom@gmail.com>
2020-07-09 07:40:51 +0700

    add bootstrap and change readme.md

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,9 @@
                     @endif
 
                     You are logged in!
+                    <div class="alert alert-primary" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
                 </div>
             </div>
         </div>
