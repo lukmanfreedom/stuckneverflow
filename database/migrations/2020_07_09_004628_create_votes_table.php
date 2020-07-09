e347d45d00d05cb7eac00aa26cf44a56897602de
LukmanFreedom <lukmanfreedom@gmail.com>
2020-07-09 07:52:49 +0700

    Add migrations for: - Questions table - Answers table - Comments table - Votes table

---
+++ database/migrations/2020_07_09_004628_create_votes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('question_id');
+            $table->bigInteger('answer_id');
+            $table->boolean('is_upvote');
+            $table->boolean('is_downvote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
